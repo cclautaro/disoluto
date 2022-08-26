2ae588e94116c2fd1a8311a869f267b8fa084afe
cclautaro <cclautaro30@gmail.com>
2022-08-26 19:59:06 -0300

    JSX & transpiling & Componentes I

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import ItemListContainer from './components/ItemListContainer';
 import NavBar from './components/NavBar';
 
 function App() {
+  const mensaje = "Hello World!"
   return (
     <div className='container-flex'>
     <NavBar/>
-    <h1>Hello world!</h1>
+    <div className='m-2'>
+    <ItemListContainer greeting={mensaje}/>
+    </div>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,22 @@
+import CartWidget from "./CartWidget"
+
 const NavBar = () => {
     return (
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand" href="#">Disoluto</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
-                        <a class="nav-link" href="#">Home</a>
-                        <a class="nav-link" href="#">Features</a>
-                        <a class="nav-link" href="#">Pricing</a>
-                        <a class="nav-link disabled">Disabled</a>
+                        <a class="nav-link">Home</a>
+                        <a class="nav-link">Productos</a>
+                        <a class="nav-link">Contacto</a>
+                        <a class="nav-link">Acerca de</a>
+                    </div>
+                    <div class="navbar-nav ms-auto">
+                        <a class="nav-link"><CartWidget/></a>
                     </div>
                 </div>
             </div>
