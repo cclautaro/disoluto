7f2eac5c4d54aed7f16b3a31a0c726d664923511
cclautaro <cclautaro30@gmail.com>
2022-09-11 01:17:19 -0300

    update

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,18 +1,16 @@
 import { useEffect, useState } from "react"
 
 const ItemDetail = ({item}) => {
-
-    useEffect(() => {
-      console.log(item);
-      console.log(item.pictureURL);
-    },[])
     
   return (
     <div>
         <ul>
             <li>{item.title}</li>
+            <li>{item.category}</li>
+            <li>{item.price}</li>
         </ul>
-        <img src={item.pictureURL} alt="modelo de silla" />
+        {/* {item.pictureURL != undefined ?<img src={require(item.pictureURL)} alt="modelo de silla" /> : null} */}
+        
     </div>
   )
 }
--- src/components/productsData.jsx
+++ src/components/productsData.jsx
@@ -21,26 +21,26 @@ const productList =
             pictureURL: "url 3"
         }
     ];
-    
-    const productData =
+
+const productData =
     [
         {
             id: 1,
-            title: "silla teca",
-            category: "silla",
+            title: "Silla Teca",
+            category: "sillas",
             price: 2100,
-            pictureURL: "../assets/Teca.jpg"
+            pictureURL: '../assets/Teca.jpg'
         },
         {
             id: 2,
-            title: "silla cesca",
-            category: "silla",
+            title: "Silla Cesca",
+            category: "sillas",
             price: 2200,
             pictureURL: "url 2"
         }, {
             id: 3,
-            title: "sillon groopie",
-            category: "sillon",
+            title: "Sofa Bubble",
+            category: "sillones",
             price: 2300,
             pictureURL: "url 3"
         }
@@ -50,4 +50,4 @@ const products = {
     productData
 }
 
-    export default products;
+export default products;
